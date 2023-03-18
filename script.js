838cb31a0128aaa43d020835a5c3d582f48aaa4c
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-03-18 15:37:35 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ inp_btn.onclick = () => {
     let inp_so = document.querySelector("select");
     console.log(inp_so.value);
     if (inp_name.value === "" || inp_comment.value == "" || inp_so.value == "") {
-        inp_name.style.border = "1px solid red";
+        inp_name.style.border = "1px solid #dc3545";
         inp_name.style.borderRight = "0px solid transparent";
-        inp_comment.style.border = "1px solid red";
-        inp_comment.style.borderRight = "5px solid red";
-        inp_btn.style.border = "1px solid red";
-        inp_btn.style.borderRight = "5px solid red";
-        inp_so.style.border = "1.5px solid red";
-        inp_so.style.borderRight = "5px solid red";
+        inp_comment.style.border = "1px solid #dc3545";
+        inp_comment.style.borderRight = "5px solid #dc3545";
+        inp_btn.style.border = "1px solid #dc3545";
+        inp_btn.style.borderRight = "5px solid #dc3545";
+        inp_so.style.border = "1.5px solid #dc3545";
+        inp_so.style.borderRight = "5px solid #dc3545";
     } else {
         inp_so.style.border = "1.5px solid #142850";
         inp_so.style.borderRight = "5px solid #142850";
